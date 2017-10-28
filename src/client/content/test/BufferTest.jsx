832d2614c7a59dc256aabeb123d9f6c7907d9ce2
BehemothTheCat <altanhir@gmail.com>
2017-10-28 19:36:49 +0200

    unity export & basic environment setup

--- src/client/content/test/BufferTest.jsx
+++ src/client/content/test/BufferTest.jsx
@@ -1,44 +1,44 @@
 import { Scene, Entity } from "aframe-react";
 import React from "react";
+import "aframe";
+import "aframe-environment-component";
 
 import preload from "../../../../public/data.json";
 
 const plankTexture = "/public/textures/WoodPlanksBare.jpg";
-
-// mergeto is bugged currently
-// <Entity
-//   id="plankyBox"
-//   geometry={{ primitive: "box", buffer: false, skipCache: true }}
-//   material={{ color: "red" }}
-//   position="0 0 0"
-// />
-// <Entity
-//   geometry={{
-//     primitive: "box",
-//     buffer: false,
-//     skipCache: true,
-//     mergeTo: "#plankyBox"
-//   }}
-//   material={{ color: "red" }}
-//   position="1 2 3"
-// />
+const unityExport = "/public/scene/scene.json";
+const unityExport2 = "/public/scene2/scene.json";
+const unityExport3 = "/public/scene3/scene.json";
+const unityExport4 = "/public/scene4/scene.json";
+const viking = "/public/VikingHouse/scene.json";
 
 class BufferTest extends React.Component {
+  componentDidMount() {
+    let targetEntity = document.querySelector("#sceneModel");
+    let mesh;
+    let loader = new THREE.ObjectLoader();
+    loader.load(unityExport3, function(object) {
+      mesh = object;
+      console.log(mesh);
+      console.log(targetEntity);
+      targetEntity.setObject3D("mesh", mesh);
+      // targetEntity.emit("model-loaded", { format: "json", model: model });
+    });
+  }
+
   render() {
     return (
       <div>
-        <Scene stats>
+        <Scene stats light="defaultLightsEnabled: false">
           <a-assets>
             <img id="plank-txtr" src={plankTexture} />
           </a-assets>
 
           <Entity
-            geometry={{
-              primitive: "box"
-            }}
-            material={{ src: "#plank-txtr" }}
-            position="0 2 -3"
+            environment={{ preset: "contact", lighting: "none", fog: 0.85 }}
           />
+
+          <Entity id="sceneModel" />
         </Scene>
       </div>
     );
